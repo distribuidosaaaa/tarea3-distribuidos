5a149b21588d7680fb42c0ac3dd5253f7b8e6195
Lukas Gutierrez Lisboa <lukasgutierrezlisboa@gmail.com>
2021-12-12 03:45:04 -0300

    working functions fulcrum

--- fulcrum/main.go
+++ fulcrum/main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"context"
 	"fmt"
 	protos "fulcrum/protobuffers"
+	"io/ioutil"
+	"log"
 	"net"
+	"os"
+	"strings"
 
 	"google.golang.org/grpc"
 )
 
 type FulcrumServer struct {
 	protos.UnimplementedFulcrumServiceServer
-	planets map[string]map[string]int
+	planets        map[string]map[string]int
+	planetVersions map[string]*[3]int
+	updateOn       int
 }
 
 func main() {
@@ -21,9 +29,10 @@ func main() {
 		panic(err)
 	}
 	m := make(map[string]map[string]int)
+	v := make(map[string]*[3]int)
 	var opts []grpc.ServerOption
 	grpcServer := grpc.NewServer(opts...)
-	server := FulcrumServer{planets: m}
+	server := FulcrumServer{planets: m, planetVersions: v, updateOn: 1}
 	protos.RegisterFulcrumServiceServer(grpcServer, &server)
 	startServer(listener, *grpcServer)
 }
@@ -38,7 +47,47 @@ func (s *FulcrumServer) AddCity(
 	ctx context.Context,
 	informanteMessage *protos.InformantMessage,
 ) (*protos.FulcrumWriteMessage, error) {
+	// verificamos si ya existe
+	_, found := s.planetVersions[informanteMessage.PlanetName]
+	if !found {
+		// si no existe el planeta inicializamos todo como corresponde
+		s.planets[informanteMessage.PlanetName] = make(map[string]int)
+		s.planetVersions[informanteMessage.PlanetName] = &[3]int{0, 0, 0}
+		f, e := os.Create(fmt.Sprintf("%v.txt", informanteMessage.PlanetName))
+		if e != nil {
+			panic(e)
+		}
+		f.Close()
+
+		fLog, e := os.Create(fmt.Sprintf("%v_log.txt", informanteMessage.PlanetName))
+		if e != nil {
+			panic(e)
+		}
+		fLog.Close()
+
+	}
+	// guardamos los datos
+	s.planetVersions[informanteMessage.PlanetName][s.updateOn]++
 	s.planets[informanteMessage.PlanetName][informanteMessage.CityName] = int(informanteMessage.NewValue)
+	planetFile, err := os.OpenFile(fmt.Sprintf("%v.txt", informanteMessage.PlanetName), os.O_APPEND|os.O_WRONLY, 0777)
+	if err != nil {
+		log.Println(err)
+	}
+	defer planetFile.Close()
+	if _, err := planetFile.WriteString(
+		fmt.Sprintf(
+			"%v %v %d",
+			informanteMessage.PlanetName,
+			informanteMessage.CityName,
+			informanteMessage.NewValue,
+		),
+	); err != nil {
+		log.Fatal(err)
+	}
+	UpdateLog(
+		fmt.Sprintf("AddCity %v %v %d", informanteMessage.PlanetName, informanteMessage.CityName, informanteMessage.NewValue),
+		informanteMessage.PlanetName,
+	)
 	return &protos.FulcrumWriteMessage{}, nil
 }
 
@@ -46,7 +95,32 @@ func (s *FulcrumServer) UpdateName(
 	ctx context.Context,
 	informanteMessage *protos.InformantStringValueMessage,
 ) (*protos.FulcrumWriteMessage, error) {
-	s.planets[informanteMessage.PlanetName][informanteMessage.CityName] = s.planets[informanteMessage.PlanetName][informanteMessage.NewValue]
+	// updateamos archivo planeta
+	fileName := fmt.Sprintf("%v.txt", informanteMessage.PlanetName)
+	input, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Aun no se ha creado una ciudad con este nombre.")
+	}
+
+	lines := strings.Split(string(input), "\n")
+
+	for i, line := range lines {
+		if strings.Contains(line, informanteMessage.CityName) {
+			lines[i] = fmt.Sprintf("%v %v %d", informanteMessage.PlanetName, informanteMessage.NewValue, s.planets[informanteMessage.PlanetName][informanteMessage.CityName])
+		}
+	}
+	output := strings.Join(lines, "\n")
+	err = ioutil.WriteFile(fileName, []byte(output), 0777)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// updateamos Log
+	UpdateLog(
+		fmt.Sprintf("UpdateName %v %v %v", informanteMessage.PlanetName, informanteMessage.CityName, informanteMessage.NewValue),
+		informanteMessage.PlanetName,
+	)
+	s.planets[informanteMessage.PlanetName][informanteMessage.NewValue] = s.planets[informanteMessage.PlanetName][informanteMessage.CityName]
+	delete(s.planets[informanteMessage.PlanetName], informanteMessage.CityName)
 	return &protos.FulcrumWriteMessage{}, nil
 }
 
@@ -54,6 +128,30 @@ func (s *FulcrumServer) UpdateNumber(
 	ctx context.Context,
 	informanteMessage *protos.InformantMessage,
 ) (*protos.FulcrumWriteMessage, error) {
+	// updateamos archivo planeta
+	fileName := fmt.Sprintf("%v.txt", informanteMessage.PlanetName)
+	input, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Aun no se ha creado una ciudad con este nombre.")
+	}
+
+	lines := strings.Split(string(input), "\n")
+
+	for i, line := range lines {
+		if strings.Contains(line, informanteMessage.CityName) {
+			lines[i] = fmt.Sprintf("%v %v %d", informanteMessage.PlanetName, informanteMessage.CityName, informanteMessage.NewValue)
+		}
+	}
+	output := strings.Join(lines, "\n")
+	err = ioutil.WriteFile(fileName, []byte(output), 0777)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// updateamos Log
+	UpdateLog(
+		fmt.Sprintf("UpdateName %v %v %v", informanteMessage.PlanetName, informanteMessage.CityName, informanteMessage.NewValue),
+		informanteMessage.PlanetName,
+	)
 	s.planets[informanteMessage.PlanetName][informanteMessage.CityName] = int(informanteMessage.NewValue)
 	return &protos.FulcrumWriteMessage{}, nil
 }
@@ -62,6 +160,48 @@ func (s *FulcrumServer) DeleteCity(
 	ctx context.Context,
 	informanteMessage *protos.InformantMessage,
 ) (*protos.FulcrumWriteMessage, error) {
+	fpath := fmt.Sprintf("%v.txt", informanteMessage.PlanetName)
+	f, err := os.Open(fpath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	var bs []byte
+	buf := bytes.NewBuffer(bs)
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		if !strings.Contains(scanner.Text(), informanteMessage.CityName) {
+			_, err := buf.Write(scanner.Bytes())
+			if err != nil {
+				log.Fatal(err)
+			}
+			_, err = buf.WriteString("\n")
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(fpath, buf.Bytes(), 0777)
+	if err != nil {
+		log.Fatal(err)
+	}
 	delete(s.planets[informanteMessage.PlanetName], informanteMessage.CityName)
 	return &protos.FulcrumWriteMessage{}, nil
+}
+
+func UpdateLog(action string, planetName string) {
+	logFile, err := os.OpenFile(fmt.Sprintf("%v_log.txt", planetName), os.O_APPEND|os.O_WRONLY, 0777)
+	if err != nil {
+		log.Println(err)
+	}
+	defer logFile.Close()
+	if _, err := logFile.WriteString(action); err != nil {
+		log.Fatal(err)
+	}
 }
